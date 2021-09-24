46c9a79841cc9c97b5bac44a59021d6839bbe86d
Carlos Cruz <70996545+ddfkr2894@users.noreply.github.com>
2021-09-24 00:15:49 -0500

    MySQL Connection

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+
+const routes = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 4000);
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'vansOFFthewall123',
+    database: 'library'
+}
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido a esta API');
+// Midlewares
+app.use(myconn(mysql, dbOptions, 'single'))
+
+// Routes
+app.get('/', (req, res)=>{
+    res.send('Welcome to my API')
 })
+app.use('/api', routes)
 
+// Running Server
 app.listen(app.get('port'), () => {
     console.log('Server corriendo en puerto', app.get('port'));
 });
